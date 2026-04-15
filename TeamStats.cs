9937152de37dde5dcf46f56410a35941402b4af5
Boootyshaker9000 <relich@post.cz>
2026-04-15 18:09:50 +0200

    Add reading team stats from external file.

---
+++ TeamStats.cs
@@ -0,0 +1,14 @@
+﻿namespace NBAMatchPredictor;
+
+public class TeamStats
+{
+    public float Pts5G { get; set; }
+    public float FgPct { get; set; }
+    public float Fg3Pct { get; set; }
+    public float Reb { get; set; }
+    public float Ast { get; set; }
+    public float Tov { get; set; }
+    public float PlusMinus { get; set; }
+    public float DaysRest { get; set; }
+    public int StarMissing { get; set; }
+}
